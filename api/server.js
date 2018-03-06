660b1112c24e2535d14c61b5cb6e2dcda6e04962
Andrew <a.massamillo@gmail.com>
2018-03-06 14:23:41 -0500

    completed stage-1

--- api/server.js
+++ api/server.js
@@ -1,12 +1,19 @@
 const express = require('express')
 const cors = require('cors')
-
+const colorRoutes = require('./routes/colors')
 const app = express()
+
 app.use(cors({ credentials: true }))
 
 // load routes here
 
 app.get('/', (req, res) => res.send('5n1 API Server'))
+
+//.... after the app.use
+
+colorRoutes(app)
+
+//.... before the app.listen
 
 app.listen(5000)
 console.log('Server listening at 5000')
